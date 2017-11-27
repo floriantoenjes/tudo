8915b6869e2b268c8040813451e7bc6b92821c15
Florian Tönjes <skatepainter@yahoo.de>
2017-11-27 20:52:16 +0100

    Removed TodoService

--- src/main/java/com/floriantoenjes/tudo/config/SecurityConfig.java
+++ src/main/java/com/floriantoenjes/tudo/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.floriantoenjes.tudo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().hasRole("USER")
-        .and().httpBasic();
+                .anyRequest()
+                .authenticated()
+        .and()
+                .httpBasic();
     }
 }
--- src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
+    @Override
+    Iterable<Todo> findAll();
 }
