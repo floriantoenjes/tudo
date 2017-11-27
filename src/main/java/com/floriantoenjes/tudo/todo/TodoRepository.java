74ef22d7699f419e005869e1275353b4d0d7a6f3
Florian Tönjes <skatepainter@yahoo.de>
2017-11-27 22:50:40 +0100

    Switched from PostAuthorize to PreAuthorize for creator

--- src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
     @Override
     Iterable<Todo> findAll();
 
-    @PostAuthorize("returnObject!=null?returnObject.iterator().hasNext()==true&&returnObject.iterator().next().creator.username==principal.username:true")
+    @PreAuthorize("#creator.username == authentication.name")
     Iterable<Todo> findAllByCreator(@Param("creator") User creator);
 }
