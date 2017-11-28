bf0bb366c2eee4de7b103c675237f244a7bfb800
Florian Tönjes <skatepainter@yahoo.de>
2017-11-28 20:59:06 +0100

    Added null check

--- src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
@@ -3,8 +3,6 @@ package com.floriantoenjes.tudo.todo;
 import com.floriantoenjes.tudo.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
@@ -12,6 +10,6 @@ public interface TodoRepository extends CrudRepository<Todo, Long> {
     @Override
     Iterable<Todo> findAll();
 
-    @PreAuthorize("#creator.username == authentication.name")
+    @PreAuthorize("#creator != null && #creator.username == authentication.name")
     Iterable<Todo> findAllByCreator(@Param("creator") User creator);
 }
