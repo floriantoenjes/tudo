f55524520a509a24b65841f22d228ae762c3fe43
Florian Tönjes <skatepainter@yahoo.de>
2017-11-29 12:11:46 +0100

    Overrode CRUD-Repository methods

--- src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
@@ -3,13 +3,42 @@ package com.floriantoenjes.tudo.todo;
 import com.floriantoenjes.tudo.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
     @Override
+    @Secured("ROLE_ADMIN")
     Iterable<Todo> findAll();
+
+    @Override
+    @Secured("ROLE_ADMIN")
+    Iterable<Todo> findAll(Iterable<Long> longs);
+
+    @Override
+    @PostAuthorize("returnObject.creator.username == authentication.name")
+    Todo findOne(Long aLong);
+
+    @Override
+    @PreAuthorize("#entity.creator.username == authentication.name")
+    void delete(@Param("entity") Todo entity);
+
+    @Override
+    @Secured("ROLE_ADMIN")
+    void delete(Long aLong);
+
+    @Override
+    @Secured("ROLE_ADMIN")
+    void delete(Iterable<? extends Todo> entities);
+
+    @Override
+    @Secured("ROLE_ADMIN")
+    void deleteAll();
 
     @PreAuthorize("#creator != null && #creator.username == authentication.name")
     Iterable<Todo> findAllByCreator(@Param("creator") User creator);
+
+
 }
