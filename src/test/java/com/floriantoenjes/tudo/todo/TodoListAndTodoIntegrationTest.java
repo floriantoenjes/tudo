dc4d4c42711cd270dd013bb5cb7ec160780e7c5f
Florian Tönjes <skatepainter@yahoo.de>
2017-11-29 20:02:34 +0100

    Fixed tests

--- src/test/java/com/floriantoenjes/tudo/todo/TodoListAndTodoIntegrationTest.java
+++ src/test/java/com/floriantoenjes/tudo/todo/TodoListAndTodoIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -58,8 +59,8 @@ public class TodoListAndTodoIntegrationTest {
         testUser = createTestUser("test_user");
     }
 
-    // ToDo: Might dirty context
     @Test
+    @DirtiesContext
     public void shouldAddTodoToTodoList() throws Exception {
         createTestTodoList("test_todo_list");
         createTestTodo("test_todo");
