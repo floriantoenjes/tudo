eaf31e526e100603baa4d6d70937c7da4de8234d
Florian Tönjes <skatepainter@yahoo.de>
2017-11-29 16:21:55 +0100

    Started implementing todo and todolist integration tests

---
+++ src/test/java/com/floriantoenjes/tudo/todo/TodoListAndTodoIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.floriantoenjes.tudo.todo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WithMockUser
+public class TodoListAndTodoIntegrationTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldAddTodoToTodoList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/todoLists/1").contentType("text/uri-list"))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
