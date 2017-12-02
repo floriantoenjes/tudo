88924c1527618cbdf98a6893e4c82bd03f745f48
Florian Tönjes <skatepainter@yahoo.de>
2017-12-02 18:45:13 +0100

    Added ContactRequestRepository.

---
+++ src/main/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepository.java
@@ -0,0 +1,6 @@
+package com.floriantoenjes.tudo.contactrequest;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactRequestRepository extends CrudRepository<ContactRequest, Long> {
+}
