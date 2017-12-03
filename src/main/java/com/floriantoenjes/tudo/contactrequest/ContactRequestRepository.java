c891b1123d78dec8ba829e6f7ad0bc7306bab583
Florian Tönjes <skatepainter@yahoo.de>
2017-12-03 13:28:17 +0100

    Further implemented ContactRequestRepository tests.

--- src/main/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepository.java
+++ src/main/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepository.java
@@ -2,7 +2,6 @@ package com.floriantoenjes.tudo.contactrequest;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -21,4 +20,31 @@ public interface ContactRequestRepository extends CrudRepository<ContactRequest,
     @PostAuthorize("returnObject.sender.username == authentication.name " +
             "|| returnObject.receiver.username == authentication.name")
     ContactRequest findOne(@Param("contactRequestId") Long aLong);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    Iterable<ContactRequest> findAll();
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    Iterable<ContactRequest> findAll(Iterable<Long> longs);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Long aLong);
+
+    @Override
+    @PreAuthorize("#entity.sender.username == authentication.name")
+    void delete(@Param("entity") ContactRequest entity);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Iterable<? extends ContactRequest> entities);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void deleteAll();
+
+
+
 }
--- src/test/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepositoryTest.java
+++ src/test/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -15,6 +16,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
@@ -43,6 +45,38 @@ public class ContactRequestRepositoryTest {
     @Test
     public void findOneWithCorrectUserShouldReturnContactRequest() throws Exception {
         mockMvc.perform(get("/api/v1/contactRequests/1").with(httpBasic("user2", "password")))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void deleteWithWrongUserShouldReturnUnauthorized() throws Exception {
+        mockMvc.perform(delete("/api/v1/contactRequests/1").with(httpBasic("user2", "password")))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void deleteWithCorrectUserShouldReturnOk() throws Exception {
+        mockMvc.perform(delete("/api/v1/contactRequests/1").with(httpBasic("user", "password")))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void findAllWithUserRoleShouldReturnUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/v1/contactRequests").with(httpBasic("user", "password")))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void findAllBySenderIdAndReceiverIdWithWrongUserShouldReturnUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/v1/contactRequests/search/findAllBySenderIdAndReceiverId?senderId=1&receiverId=2").with(httpBasic("user3", "password")))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+//    ToDo: Investigate why this test fails with 'user' instead of 'user2'
+    @Test
+    public void findAllBySenderIdAndReceiverIdWithCorrectUserShouldReturnContactRequests() throws Exception {
+        mockMvc.perform(get("/api/v1/contactRequests/search/findAllBySenderIdAndReceiverId?senderId=1&receiverId=2").with(httpBasic("user2", "password")))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
