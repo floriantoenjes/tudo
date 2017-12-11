d593021039c7b6b45533e85c06477ae7aa7b1949
Florian Tönjes <skatepainter@yahoo.de>
2017-12-11 10:09:35 +0100

    JWT authentication working.

--- src/main/java/com/floriantoenjes/tudo/config/SecurityConfig.java
+++ src/main/java/com/floriantoenjes/tudo/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.floriantoenjes.tudo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,17 +20,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers(HttpMethod.OPTIONS, "/**")
                     .permitAll()
+                .antMatchers(HttpMethod.POST, "/login")
+                .permitAll()
                     .anyRequest()
                     .authenticated()
                 .and()
+                    .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                            UsernamePasswordAuthenticationFilter.class)
+                    .addFilterBefore(new JWTAuthenticationFilter(),
+                            UsernamePasswordAuthenticationFilter.class)
                     .headers()
                     .frameOptions()
                     .disable()
                 .and()
                     .httpBasic()
                     .and()
-//                    .cors().disable()
                     .csrf()
                     .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
     }
 }
