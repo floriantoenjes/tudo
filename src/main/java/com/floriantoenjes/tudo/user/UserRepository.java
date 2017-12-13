68a0c559b81318b1a4c152c79510f8157743ee4b
Florian Tönjes <skatepainter@yahoo.de>
2017-12-13 11:30:42 +0100

    Replaced CrudRepository with PagingAndSortingRepository.

--- src/main/java/com/floriantoenjes/tudo/user/UserRepository.java
+++ src/main/java/com/floriantoenjes/tudo/user/UserRepository.java
@@ -1,8 +1,8 @@
 package com.floriantoenjes.tudo.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User findByUsername(String username);
 
 }
