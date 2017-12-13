eefaf0a27b946a772530d0f608991dbf4317119a
Florian Tönjes <skatepainter@yahoo.de>
2017-12-13 11:06:19 +0100

    Optimized imports.

--- src/main/java/com/floriantoenjes/tudo/user/User.java
+++ src/main/java/com/floriantoenjes/tudo/user/User.java
@@ -12,9 +12,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.Configuration;
 import javax.validation.constraints.NotNull;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @ToString(exclude = {"todos", "todoLists", "assignedTodos",
