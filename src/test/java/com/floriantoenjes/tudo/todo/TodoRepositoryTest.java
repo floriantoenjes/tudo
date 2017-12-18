fa5729ef8bb0ebb8b0bcdd99cbaf9c45a8a19666
Florian Tönjes <skatepainter@yahoo.de>
2017-12-18 12:48:48 +0100

    All TodoRepository tests green again.

--- src/test/java/com/floriantoenjes/tudo/todo/TodoRepositoryTest.java
+++ src/test/java/com/floriantoenjes/tudo/todo/TodoRepositoryTest.java
@@ -12,16 +12,12 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
@@ -65,7 +61,9 @@ public class TodoRepositoryTest {
 
     @Test
     public void findOneWithCorrectUserShouldReturnTodo() throws Exception {
-        mockMvc.perform(get("/api/v1/todos/1").with(httpBasic("user", "password")))
+        String userJwt = getJwtToken("user", "password");
+
+        mockMvc.perform(get("/api/v1/todos/1").header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/hal+json;charset=UTF-8"));
     }
@@ -73,21 +71,27 @@ public class TodoRepositoryTest {
 
     @Test
     public void deleteWithWrongUserShouldReturnUnauthorized() throws Exception {
-        mockMvc.perform(delete("/api/v1/todos/1").with(httpBasic("user2", "password")))
+        String user2jwt = getJwtToken("user2", "password");
+
+        mockMvc.perform(delete("/api/v1/todos/1").header("Authorization", user2jwt))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     public void deleteWithCorrectUserShouldReturnOk() throws Exception {
-        mockMvc.perform(delete("/api/v1/todos/1").with(httpBasic("user", "password")))
+        String userJwt = getJwtToken("user", "password");
+
+        mockMvc.perform(delete("/api/v1/todos/1").header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
 
     @Test
     public void findAllWithUserRoleShouldReturnUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/v1/todos").with(httpBasic("user", "password")))
+        String userJwt = getJwtToken("user", "password");
+
+        mockMvc.perform(get("/api/v1/todos").header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
@@ -100,16 +104,19 @@ public class TodoRepositoryTest {
 
     @Test
     public void findAllByCreatorWithWrongUserShouldReturnUnauthorized() throws Exception {
+        String user2jwt = getJwtToken("user2", "password");
+
         mockMvc.perform(get("/api/v1/todos/search/findAllByCreator?creator=/api/v1/users/1")
-                .with(httpBasic("user2", "password")))
-                .andExpect(MockMvcResultMatchers.status().isForbidden())
-                .andDo(MockMvcResultHandlers.print());
+                .header("Authorization", user2jwt))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void findAllByCreatorWithCorrectUserShouldReturnTodos() throws Exception {
+        String userJwt = getJwtToken("user", "password");
+
         mockMvc.perform(get("/api/v1/todos/search/findAllByCreator?creator=/api/v1/users/1")
-                .with(httpBasic("user", "password")))
+                .header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/hal+json;charset=UTF-8"));
     }
@@ -117,24 +124,29 @@ public class TodoRepositoryTest {
 
     @Test
     public void findAllByCreatorAndTagsWithWrongUserAndExistingTagShouldReturnUnauthorized() throws Exception {
+        String user2jwt = getJwtToken("user2", "password");
+
         mockMvc.perform(get("/api/v1/todos/search/findAllByCreatorAndTags?creator=/api/v1/users/1&tag=tag")
-                .with(httpBasic("user2", "password")))
+                .header("Authorization", user2jwt))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void findAllByCreatorAndTagsWithCorrectUserAndNotExistingTagShouldReturnEmpty() throws Exception {
+        String userJwt = getJwtToken("user", "password");
+
         mockMvc.perform(get("/api/v1/todos/search/findAllByCreatorAndTags?creator=/api/v1/users/1&tag=invalid")
-                .with(httpBasic("user", "password")))
-                .andDo(MockMvcResultHandlers.print())
+                .header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$._embedded.todos", hasSize(0)));
     }
 
     @Test
     public void findAllByCreatorAndTagsWithCorrectUserAndExistingTagShouldReturnTodos() throws Exception {
+        String userJwt = getJwtToken("user", "password");
+
         mockMvc.perform(get("/api/v1/todos/search/findAllByCreatorAndTags?creator=/api/v1/users/1&tag=tag")
-                .with(httpBasic("user", "password")))
+                .header("Authorization", userJwt))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/hal+json;charset=UTF-8"));
     }
