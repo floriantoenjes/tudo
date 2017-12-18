e5079b60eb59ea10930f69ecbf2f58348fae83a3
Florian Tönjes <skatepainter@yahoo.de>
2017-12-18 17:03:12 +0100

    Reorganized imports.

--- src/test/java/com/floriantoenjes/tudo/todo/TodoListAndTodoIntegrationTest.java
+++ src/test/java/com/floriantoenjes/tudo/todo/TodoListAndTodoIntegrationTest.java
@@ -1,10 +1,6 @@
 package com.floriantoenjes.tudo.todo;
 
-import com.floriantoenjes.tudo.user.RoleRepository;
-import com.floriantoenjes.tudo.user.User;
-import com.floriantoenjes.tudo.user.UserRepository;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,17 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.NestedServletException;
 
 import static com.floriantoenjes.tudo.TestUtils.getJwtToken;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
