82ef7fdbec9ca951f9796139f048f6c6fe03212e
Florian Tönjes <skatepainter@yahoo.de>
2017-12-18 12:35:30 +0100

    First test with JWT green again.

--- src/test/java/com/floriantoenjes/tudo/todo/TodoRepositoryTest.java
+++ src/test/java/com/floriantoenjes/tudo/todo/TodoRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,6 +21,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
@@ -43,9 +46,20 @@ public class TodoRepositoryTest {
 
 //  ToDo: Write tests for PUT method
 
+    private String getJwtToken(String username, String password) throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/v1/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}"))
+                .andReturn();
+
+        return result.getResponse().getHeader("Authorization");
+    }
+
     @Test
     public void findOneWithWrongUserShouldReturnUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/v1/todos/1").with(httpBasic("user2", "password")))
+        String user2jwt = getJwtToken("user2", "password");
+
+        mockMvc.perform(get("/api/v1/todos/1").header("Authorization", user2jwt))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
