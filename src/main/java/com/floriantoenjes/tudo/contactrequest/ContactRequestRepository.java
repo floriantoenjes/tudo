d465df4bf2da909ef3501be577b7f88bd526c4f4
Florian Tönjes <skatepainter@yahoo.de>
2018-10-13 17:40:49 +0200

    Set @PreAuthorize() at class level

--- src/main/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepository.java
+++ src/main/java/com/floriantoenjes/tudo/contactrequest/ContactRequestRepository.java
@@ -7,46 +7,45 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface ContactRequestRepository extends CrudRepository<ContactRequest, Long> {
 
     @Override
-    @PreAuthorize("!#entity.receiver.username.equals(authentication.name)")
+    @PreAuthorize("(!#entity.receiver.username.equals(authentication.name)) || hasRole('ROLE_ADMIN')")
     <S extends ContactRequest> S save(@Param("entity") S entity);
 
-    @PreAuthorize("authentication.name.equals(#receiverName)")
+    @PreAuthorize("authentication.name.equals(#receiverName) || hasRole('ROLE_ADMIN')")
     Iterable<ContactRequest> findAllByReceiverUsername(@Param("receiverName") String receiverName);
 
-    @PreAuthorize("authentication.name.equals(#senderName) || authentication.name.equals(#receiverName)")
+    @PreAuthorize("authentication.name.equals(#senderName) || authentication.name.equals(#receiverName) " +
+            "|| hasRole('ROLE_ADMIN')")
     ContactRequest findBySenderUsernameAndReceiverUsername(@Param("senderName") String senderName,
                                                            @Param("receiverName") String receiverName);
 
     @Override
+    @PreAuthorize("permitAll()")
     @PostAuthorize("returnObject != null && (returnObject.sender.username == authentication.name " +
             "|| returnObject.receiver.username == authentication.name)")
     ContactRequest findOne(@Param("contactRequestId") Long aLong);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<ContactRequest> findAll();
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<ContactRequest> findAll(Iterable<Long> longs);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Long aLong);
 
     @Override
-    @PreAuthorize("#entity.sender.username == authentication.name || #entity.receiver.username == authentication.name")
+    @PreAuthorize("#entity.sender.username == authentication.name || #entity.receiver.username == authentication.name " +
+            "|| hasRole('ROLE_ADMIN')")
     void delete(@Param("entity") ContactRequest entity);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Iterable<? extends ContactRequest> entities);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void deleteAll();
 
 
