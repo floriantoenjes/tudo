88054aa5752ff83d7d3f51a68f5aefe81ec92571
Florian Tönjes <skatepainter@yahoo.de>
2018-10-13 17:18:10 +0200

    Using preAuthorize at class level now

--- src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoRepository.java
@@ -6,55 +6,49 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-
-//    ToDo: Change the access for admins
 
     @Override
     @PreAuthorize("#entity.creator.username == authentication.name || hasRole('ROLE_ADMIN')")
     <S extends Todo> S save(@Param("entity") S entity);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     <S extends Todo> Iterable<S> save(Iterable<S> entities);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<Todo> findAll();
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<Todo> findAll(Iterable<Long> longs);
 
     @Override
+    @PreAuthorize("permitAll()")
     @PostAuthorize("returnObject != null && returnObject.creator.username == authentication.name " +
             "|| returnObject.isAssignedToUser(authentication.name)")
     Todo findOne(Long aLong);
 
     @Override
-    @PreAuthorize("#entity != null && #entity.creator.username == authentication.name")
+    @PreAuthorize("(#entity != null && #entity.creator.username == authentication.name) || hasRole('ROLE_ADMIN')")
     void delete(@Param("entity") Todo entity);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Long aLong);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Iterable<? extends Todo> entities);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void deleteAll();
 
-    @PreAuthorize("#creator != null && #creator.username == authentication.name")
+    @PreAuthorize("(#creator != null && #creator.username == authentication.name) || hasRole('ROLE_ADMIN')")
     Iterable<Todo> findAllByCreator(@Param("creator") User creator);
 
 
-    @PreAuthorize("#creator != null && #creator.username == authentication.name")
+    @PreAuthorize("(#creator != null && #creator.username == authentication.name) || hasRole('ROLE_ADMIN')")
     Iterable<Todo> findAllByCreatorAndTags(@Param("creator") User creator, @Param("tag") String tag);
 
-    @PreAuthorize("#assignee.username == authentication.name")
+    @PreAuthorize("(#assignee.username == authentication.name) || hasRole('ROLE_ADMIN')")
     Iterable<Todo> findAllByAssignedUsersContaining(@Param("assignee") User assignee);
 
 }
