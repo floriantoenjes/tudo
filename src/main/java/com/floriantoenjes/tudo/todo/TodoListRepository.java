8e041c721ec4593e7abe37ac58ff1a42d34c475c
Florian Tönjes <skatepainter@yahoo.de>
2018-10-13 17:05:21 +0200

    Using preAuthorize at class level now

--- src/main/java/com/floriantoenjes/tudo/todo/TodoListRepository.java
+++ src/main/java/com/floriantoenjes/tudo/todo/TodoListRepository.java
@@ -6,37 +6,34 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface TodoListRepository extends CrudRepository<TodoList, Long> {
 
     @Override
+    @PreAuthorize("permitAll()")
     @PostAuthorize("returnObject != null && returnObject.creator.username == authentication.name")
     TodoList findOne(Long aLong);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<TodoList> findAll();
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     Iterable<TodoList> findAll(Iterable<Long> longs);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Long aLong);
 
     @Override
-    @PreAuthorize("#entity != null && #entity.creator.username == authentication.name")
+    @PreAuthorize("(#entity != null && #entity.creator.username == authentication.name) || hasRole('ROLE_ADMIN')")
     void delete(@Param("entity") TodoList entity);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void delete(Iterable<? extends TodoList> entities);
 
     @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     void deleteAll();
 
-    @PreAuthorize("#creator != null && #creator.username == authentication.name")
+    @PreAuthorize("(#creator != null && #creator.username == authentication.name) || hasRole('ROLE_ADMIN')")
     Iterable<TodoList> findAllByCreator(@Param("creator") User creator);
 
 }
