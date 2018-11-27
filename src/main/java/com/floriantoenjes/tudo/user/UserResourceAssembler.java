93eb9d8c692be648006732c3eb930f135a9dd95d
Florian Tönjes <skatepainter@yahoo.de>
2018-11-27 21:38:10 +0100

    Getting base path from project settings now

--- src/main/java/com/floriantoenjes/tudo/user/UserResourceAssembler.java
+++ src/main/java/com/floriantoenjes/tudo/user/UserResourceAssembler.java
@@ -1,11 +1,15 @@
 package com.floriantoenjes.tudo.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserResourceAssembler extends ResourceAssemblerSupport<User, UserResource> {
+
+    @Value("${spring.data.rest.basePath}")
+    private String apiBasePath;
 
     public UserResourceAssembler(){
         super(UserController.class, UserResource.class);
@@ -17,7 +21,7 @@ public class UserResourceAssembler extends ResourceAssemblerSupport<User, UserRe
         userResource.setUsername(entity.getUsername());
 
         userResource.add(ControllerLinkBuilder.linkTo(UserController.class)
-                .slash("api/v1/users").slash(entity).withSelfRel());
+                .slash(apiBasePath).slash(entity).withSelfRel());
 
         return userResource;
     }
